90ddf268e27166489d993663a9449dc5902de95b
raccoon <donogo.umrahil@yandex.ru>
2023-09-06 20:41:16 +0300

    it was easy

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов,
+//стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+
+int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(i < arr.Length - 1)  Console.Write($"{arr[i]}, ");
+        else Console.Write($"{arr[i]}");
+    }
+    Console.Write("]");
+}
+
+int[] array = CreateArrayRndInt(4, 100, 1000);
+PrintArray(array);
+
+int UnEvenSum(int[] arr)
+{
+int sum = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+if (i % 2 == 0) sum += 0;
+else sum += arr[i];
+}
+return sum;
+}
+
+int unEvenSum = UnEvenSum(array);
+Console.WriteLine($" — {unEvenSum}");
